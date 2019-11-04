df23dbd9a4b47570c29ff84b1eed7bbbfd9153d3
owner <moonyoungtae@postech.ac.kr>
2019-11-04 16:06:34 +0900

    DataConstructing codes for robotArmControl

--- Motor/Motor.py
+++ Motor/Motor.py
@@ -2,12 +2,14 @@
 # Cotrolling with A4988 and Rasberry Pi 3 B
 
 import sys
-#import RPi.GPIO as gpio  # https://pypi.python.org/pypi/RPi.GPIO more info
+import RPi.GPIO as gpio  # https://pypi.python.org/pypi/RPi.GPIO more info
 import time
+import math
+
 
 class Motor:
 
-    def __init__(self, angPerSt, gear,maxAng=0.0, minAng=0.0):
+    def __init__(self, angPerSt, gear, maxAng=0.0, minAng=0.0):
         self.dir = 'right'  # direction of rotate, right=clockwise
         self.curAng = 0.00  # current angle, For robot arm positioning & coorection error of movility
         self.angPerSt = angPerSt  # stepper motor configuration
@@ -17,7 +19,7 @@ class Motor:
         self.dirPin = 0
         self.stpPin = 0
 
-    def set(self,dirPin, stpPin):
+    def set(self, dirPin, stpPin):
         gpio.setmode(gpio.BCM)
         gpio.setup(dirPin, gpio.OUT)  # setting gpio, dirPin controls direction of motor
         gpio.setup(stpPin, gpio.OUT)  # stpPin controls step number of motor, you must set connect pin on rasberry Pi 3B
@@ -30,7 +32,7 @@ class Motor:
 
     def getStep(self, angle):
         stp = int(angle / self.angPerSt)
-        return abs(stp)
+        return stp
 
     def getDelay(self, vel):
         # rotation per sec => step/s
@@ -38,35 +40,29 @@ class Motor:
         t = 1.000 / vel
         return t / totalStep
 
-    def getSmoothVel(self,maxVel,target,cur):
-        return 1/(1+(cur/target)(cur/target))
+    def getSmoothVel(self, maxVel, target, cur):
+        return 20 * maxVel / (19 + abs(math.sqrt(cur / target)))
 
-    def move(self, angle, vel,smooth):
-        print("motor thread")
-        return True
-
+    def move(self, angle, vel, smooth):
 
         if (angle + self.curAng < self.minAng or angle + self.curAng < self.maxAng) and self.maxAng != 0:
+            print("False")
             return False
+        print("1")
 
-        angle = (-angle)
-
-        delay = self.getDelay(vel*self.gear)
-        stp = self.getStep(angle*self.gear)
+        delay = self.getDelay(vel * self.gear)
+        stp = self.getStep(abs(angle) * self.gear)
         counter = 0
-        if(stp==0):
+        if (stp == 0):
             return False
 
-        if angle >= 0.0:
+        if angle > 0.0:
             gpio.output(self.dirPin, True)
-            self.curAng += (stp * self.angPerSt)
         else:
             gpio.output(self.dirPin, False)
-            self.curAng -= (stp * self.angPerSt)
 
-
-        if(not smooth):
-            delay=self.getDelay(vel*self.gear)
+        if (not smooth):
+            delay = self.getDelay(vel * self.gear)
             while counter < stp:
                 gpio.output(self.stpPin, True)
                 time.sleep(delay)
@@ -75,16 +71,15 @@ class Motor:
                 counter += 1
         else:
             while counter < stp:
-                v=self.getSmoothVel(vel,stp/2,stp/2-counter)
-                delay=self.getDelay(v)
+                v = self.getSmoothVel(vel, stp / 2, stp / 2 - counter)
+                delay = self.getDelay(v)
                 gpio.output(self.stpPin, True)
                 time.sleep(delay)
                 gpio.output(self.stpPin, False)
                 time.sleep(delay)
                 counter += 1
         return True
-
 
     def __del__(self):
         print()
-        #gpio.cleanup()
+        # gpio.cleanup()
--- RobotArm/RobotArmClient.py
+++ RobotArm/RobotArmClient.py
@@ -1,91 +1,150 @@
 import numpy as np
 
 from Motor import Motor
+
 import RPi.GPIO as gpio
+
 import math
+
 import operator
+
 import threading
+
 import time
+
 import queue
 
+
 class armClient:
+
     def __init__(self, dirPins, stpPins, maxAngles, minAngles, servoPin):
-        a = calAngle(49.0, -9.0)
+
+        a = calAngle(49.0, -5.0)
+
         self.curAngles = [a[0], a[1], 0.0]
+
         self.motorList = []
+
         self.slice = 50
+
         self.ratio = 5.0  # ratio of rotation,  second motor rotates robot arm indirect wat
+
         gpio.setmode(gpio.BCM)
-        gpio.setup(servoPin, gpio.OUT)
-        self.servoPwm = gpio.PWM(servoPin, 50)
-        self.servoPwm.start(0)
+
+        # gpio.setup(servoPin, gpio.OUT)
+
+        # self.servoPwm = gpio.PWM(servoPin, 50)
+
+        # self.servoPwm.start(0)
+
         for i in range(0, 3):
             motor = Motor(1.8, 10.0, 0.0, 0.0)
-            motor.set(dirPins[i], stpPins[i])
-            motor.maxAng = maxAngles[i]
-            motor.minAng = minAngles[i]
-            motor.curAng=self.curAngles[i]
-            self.motorList.append(motor)
-        self.motorList[0].gear = 50
-        self.motorList[1].curAng=self.curAngles[1]-self.curAngles[0]
 
+            motor.set(dirPins[i], stpPins[i])
+
+            motor.maxAng = maxAngles[i]
+
+            motor.minAng = minAngles[i]
+
+            motor.curAng = self.curAngles[i]
+
+            self.motorList.append(motor)
+
+        self.motorList[0].gear = 50
+
+        self.motorList[1].curAng = self.curAngles[1] + self.curAngles[0]
 
     def rotate(self, target):
+
         angle = target - self.curAngles[2]
+
         if (abs(angle) > 180.0):
             angle = angle - 360
-        self.motorList[2].move(angle, 1.0, True)
+
+        print(angle)
+
+        self.motorList[2].move(angle, 1.0, False)
+
+        self.motorList[2].curAng += (
+                    self.motorList[2].getStep(angle * self.motorList[2].gear) * 1.8 / self.motorList[2].gear)
+
         self.curAngles[2] = self.motorList[2].curAng
 
-    def moveInDirectLine(self,a1, b1, resol):
+    def moveInDirectLine(self, a1, b1, resol):
         cur = [self.curAngles[0], self.curAngles[1]]
+        print(cur)
         pos1 = calPos(cur[0], cur[1])
         pos2 = calPos(a1, b1)
         dif = [(pos2[0] - pos1[0]) / resol, (pos2[1] - pos1[1]) / resol]
-
         m = self.motorList
-
         before = [cur[0], cur[1]]
-        for i in range(0, resol):
+
+        stpArr=[]
+        velArr=[]
+        len=0
+        for i in range(1, resol + 1):
             cur = calAngle(pos1[0] + i * dif[0], pos1[1] + i * dif[1])
             # because the frame of robot arm always makes prallelogram
+            mov = [(cur[0] - before[0]), cur[1] + cur[0] - (before[1] + before[0])]
+            print(mov)
+            stp = [int(mov[0] * m[0].gear / 1.8), int(mov[1] * m[1].gear / 1.8)]
 
-            mov = [(cur[0] - before[0]), cur[1]-cur[0] - (before[1]-before[0])]
+            if(stp[0]>0 and stp[1]>0):
+                len+=1
+                stpArr.apeend(stp)
+                v1 = 0.2
+                v2 = 0.2
+                if (stp[0] > stp[1]):
+                    v2 = v1 * stp[1] / stp[0]
+                else:
+                    v1 = v2 * stp[0] / stp[1]
+                velArr.append([v1,v2])
 
-            que = queue.Queue()
-            t1 = threading.Thread(target=m[0].move, args=(-mov[0], 0.5, False))
-            t2 = threading.Thread(target=lambda q, arg1: q.put(m[1].move(arg1)), args=(que,mov[1], 1.5, False))
+            m[0].curAng += (stp[0] * 1.8 / m[0].gear)
+            m[1].curAng += (stp[1] * 1.8 / m[1].gear)
+            before = [m[0].curAng, m[1].curAng - m[0].curAng]
+
+        for i in range(0,len):
+            v=velArr[i]
+            stp=stpArr[i]
+            t1 = threading.Thread(target=m[0].move, args=(-stpArr[0]*1.8*m[0].gear, v[0], False))
+            t2 = threading.Thread(target=m[1].move, args=(stpArr[1]*1.8*m[1].gear, v[1], False))
             t1.start()
             t2.start()
             t1.join()
             t2.join()
-            before = [m[0].curAng,m[0].curAng+m[1].curAng]
 
-        self.curAngles=[m[0].curAng,m[1].curAng+m[0].curAng,m[2].curAng]
+        self.curAngles = [m[0].curAng, m[1].curAng - m[0].curAng, m[2].curAng]
 
     def work(self, target, lift):
+        time.sleep(2)
         cur = [self.curAngles[0], self.curAngles[1], self.curAngles[2]]
         self.rotate(target[2])
-        self.moveInDirectLine(target[0], target[1], 5)
+        time.sleep(0.5)
+        self.moveInDirectLine(target[0], target[1], 1)
         if lift:
             pass
-            #self.servoPwm.ChangeDutyCycle(3)
+            # self.servoPwm.ChangeDutyCycle(3)
         else:
             pass
-            #self.servoPwm.ChangeDutyCycle(12)
-        time.sleep(1)
-        self.moveInDirectLine(cur[0],cur[1],5)
+            # self.servoPwm.ChangeDutyCycle(12)
+        time.sleep(2)
+        print(self.curAngles)
+        self.moveInDirectLine(cur[0], cur[1], 1)
+        time.sleep(0.5)
+        self.rotate(cur[2])
 
     def initPos(self):
         pass
 
-l1 = 140
+l1 = 145
 l2 = 180
 
 def calPos(a, b):
     # 3 cases
     return [l1 * math.cos(a * math.pi / 180) + l2 * math.cos((a + b - 180) * math.pi / 180),
             l1 * math.sin(a * math.pi / 180) + l2 * math.sin((a + b - 180) * math.pi / 180)]
+
 
 def calAngle(x, y):
     l3 = math.sqrt(x ** 2 + y ** 2)
@@ -96,7 +155,10 @@ def calAngle(x, y):
     return [(tan + a2) * 180 / math.pi, a3 * 180 / math.pi]
 
 
-test = armClient([21, 24, 27], [20, 23, 17], [0.0, 0.0, 0.0], [360.0, 360.0, 360.0], 10)
-print(test.curAngles)
-test.moveInDirectLine(test.curAngles[0], test.curAngles[1], 0.0, 50.0, 1000)
-
+test = armClient([21, 24, 15], [20, 23, 14], [0.0, 0.0, 0.0], [360.0, 360.0, 360.0], 10)
+ang = calAngle(100.0, -71.0)
+print(ang)
+print(calPos(ang[0], ang[1]))
+test.work([ang[0], ang[1], 50.0], False)
+ang = calAngle(170.0, 0.0)
+test.work([ang[0], ang[1], 50.0], False)
