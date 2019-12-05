11b192475d446900cb2e5ec1a9c3f1e2d6d96824
Mintae Kim <shsesther928@postech.ac.kr>
2019-12-06 01:38:14 +0900

    Test Purpose

---
+++ Car_Test_Main.py
@@ -0,0 +1,24 @@
+from Car import Car
+import time
+
+car = Car()
+
+while(1):
+    time.sleep(1)
+    test=input()
+    if test=="w" :
+        car.move_forward(10000)
+    elif (test=="s"):
+        car.move_forward(-600)
+    elif test=="d":
+        car.move_right(300)
+    elif(test=="a"):
+        car.move_right(-300)
+    elif(test=="e"):
+        car.rotate(500)
+    elif(test=="q"):
+        car.rotate(-500)
+    elif(test=="stop"):
+        break
+    else:
+        continue
